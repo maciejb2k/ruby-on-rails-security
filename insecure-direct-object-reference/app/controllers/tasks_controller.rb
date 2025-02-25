1ce58cbd98e35305e37ef825ba3d2178db1dc4d1
Maciej Biel <maciek.b2k@gmail.com>
2025-02-25 22:33:09 +0100

    refactor: refactor TasksController to improve code readability and maintainability

--- insecure-direct-object-reference/app/controllers/tasks_controller.rb
+++ insecure-direct-object-reference/app/controllers/tasks_controller.rb
@@ -1,17 +1,16 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_task, only: %i[ show edit update destroy secure_show_pundit ]
+  before_action :set_task, only: %i[show edit update destroy secure_show_pundit]
 
   def index
     @tasks = policy_scope(Task)
   end
 
-  def show
-  end
-
   def new
     @task = current_user.tasks.build
   end
+
+  def show; end
 
   def secure_show
     @task = current_user.tasks.find_by(id: params[:id])
@@ -25,11 +24,13 @@ class TasksController < ApplicationController
 
   def secure_show_pundit
     authorize @task, :show?
+
     render :show
   end
 
   def edit
     authorize @task
+
     render :edit
   end
 
@@ -37,7 +38,7 @@ class TasksController < ApplicationController
     @task = current_user.tasks.build(task_params)
 
     if @task.save
-      redirect_to @task, notice: "Task was successfully created."
+      redirect_to @task, notice: 'Task was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -47,7 +48,7 @@ class TasksController < ApplicationController
     authorize @task
 
     if @task.update(task_params)
-      redirect_to @task, notice: "Task was successfully updated.", status: :see_other
+      redirect_to @task, notice: 'Task was successfully updated.', status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
@@ -57,15 +58,16 @@ class TasksController < ApplicationController
     authorize @task
 
     @task.destroy!
-    redirect_to tasks_url, notice: "Task was successfully destroyed.", status: :see_other
+    redirect_to tasks_url, notice: 'Task was successfully destroyed.', status: :see_other
   end
 
   private
-    def set_task
-      @task = Task.find(params[:id])
-    end
 
-    def task_params
-      params.require(:task).permit(:title, :description, :user_id)
-    end
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :user_id)
+  end
 end
